be6fdc8c38eae1d787748b28d3f8bd69cb7df5b3
TheRezorBl-de <136909096+TheRezorBl-de@users.noreply.github.com>
2023-09-24 23:45:22 +0530

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -43,20 +43,6 @@ public class BankAccount {
     }
 }
 
-class Transaction {
-    private String type;
-    private double amount;
 
-    public Transaction(String type, double amount) {
-        this.type = type;
-        this.amount = amount;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-}
+        
+    
